74367856b1a704e48f0b7f75b5f94a2b12c50a0c
Jihye Eom <jeom327@outlook.com>
2019-01-17 15:34:59 +0900

    edited the echo in the pre-build script

--- Droid/appcenter-pre-build.sh
+++ Droid/appcenter-pre-build.sh
@@ -2,4 +2,4 @@
 
 sed -i -e "s/\[PLATFORM]/$VSAC_Env/g" ../Constants.cs
 
-echo "The Environment variable is successfully set to -> $VSAC_Env"
+echo "The Environment variable is successfully set to "+"->"+ "$VSAC_Env"
